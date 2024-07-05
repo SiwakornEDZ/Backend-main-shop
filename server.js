1d9fcfc99a2634c9a77f632e3fff60211e168c5c
siwakorn.k <siwakorn.k@blendata.co>
2024-07-05 20:35:05 +0700

    update

--- server.js
+++ server.js
@@ -17,34 +17,34 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(cookieParser())
 
 
-// app.use('/auth', require('./routes/authRoute'))
-// app.use('/users', require('./routes/userRoute'))
-// app.use('/products', require('./routes/productRoute'))
-// app.use('/carts', require('./routes/cartRoute'))
-// app.use('/orders', require('./routes/orderRoute'))
-// app.use('/coupons', require('./routes/couponRoute'))
+app.use('/auth', require('./routes/authRoute'))
+app.use('/users', require('./routes/userRoute'))
+app.use('/products', require('./routes/productRoute'))
+app.use('/carts', require('./routes/cartRoute'))
+app.use('/orders', require('./routes/orderRoute'))
+app.use('/coupons', require('./routes/couponRoute'))
 
-// app.use(notFound)
-// app.use(errorHandler)
-// app.get("/", (req, res) => { res.send("Express on Vercel"); });
+app.use(notFound)
+app.use(errorHandler)
+app.get("/", (req, res) => { res.send("Express on Vercel"); });
 
-// app.listen(port, ()=>{
-//     connectDb()
-//     console.log(`Listening to ${port}`);
-// })
+app.listen(port, ()=>{
+    connectDb()
+    console.log(`Listening to ${port}`);
+})
 
-// module.exports = app
+module.exports = app
 
-const server = http.createServer((req, res) => {
-    // Set the response header
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    // Write some text to the response
-    res.end('Welcome to my simple Node.js app!');
-});
+// const server = http.createServer((req, res) => {
+//     // Set the response header
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     // Write some text to the response
+//     res.end('Welcome to my simple Node.js app!');
+// });
  
-// Define the port to listen on
+// // Define the port to listen on
  
-// Start the server
-server.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-});
+// // Start the server
+// server.listen(port, () => {
+//     console.log(`Server is running on http://localhost:${port}`);
+// });
