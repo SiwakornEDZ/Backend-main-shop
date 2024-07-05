9b134c9a96b04d2086324883c51936364e77e3a8
siwakorn.k <siwakorn.k@blendata.co>
2024-07-05 20:29:21 +0700

    test

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const http = require('http');
 const morgan = require('morgan')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
@@ -15,20 +16,35 @@ app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(cookieParser())
 
-app.use('/auth', require('./routes/authRoute'))
-app.use('/users', require('./routes/userRoute'))
-app.use('/products', require('./routes/productRoute'))
-app.use('/carts', require('./routes/cartRoute'))
-app.use('/orders', require('./routes/orderRoute'))
-app.use('/coupons', require('./routes/couponRoute'))
 
-app.use(notFound)
-app.use(errorHandler)
-app.get("/", (req, res) => { res.send("Express on Vercel"); });
+// app.use('/auth', require('./routes/authRoute'))
+// app.use('/users', require('./routes/userRoute'))
+// app.use('/products', require('./routes/productRoute'))
+// app.use('/carts', require('./routes/cartRoute'))
+// app.use('/orders', require('./routes/orderRoute'))
+// app.use('/coupons', require('./routes/couponRoute'))
 
-app.listen(port, ()=>{
-    connectDb()
-    console.log(`Listening to ${port}`);
-})
+// app.use(notFound)
+// app.use(errorHandler)
+// app.get("/", (req, res) => { res.send("Express on Vercel"); });
 
-module.exports = app
+// app.listen(port, ()=>{
+//     connectDb()
+//     console.log(`Listening to ${port}`);
+// })
+
+// module.exports = app
+
+const server = http.createServer((req, res) => {
+    // Set the response header
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    // Write some text to the response
+    res.end('Welcome to my simple Node.js app!');
+});
+ 
+// Define the port to listen on
+ 
+// Start the server
+server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
