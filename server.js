a01c0a53d1c15cfea716841858f0a0484a5084ef
siwakorn.k <siwakorn.k@blendata.co>
2024-07-05 20:48:49 +0700

    update

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express()
 
 const port = process.env.PORT || 5050
 app.use(cors())
-//app.use(morgan('dev'))
+//app.use(morgan('dev')).
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(cookieParser())
